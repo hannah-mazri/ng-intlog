acaf768927cfc62ee72ba66c8f1a59a582fd56ba
hannah <hannahnajwa@gmail.com>
2021-07-22 14:59:34 +0800

    new component: calendar-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarContainerComponent } from './calendar-container/calendar-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarContainerComponent
   ],
   imports: [
     BrowserModule,
